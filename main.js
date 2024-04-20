66d6ea0f5ac14b1f0f4439e6961ef6313fc476a1
Réka Hajdú <hajdu.reka1109@gmail.com>
2024-04-20 17:19:54 +0200

    language sidebar

--- main.js
+++ main.js
@@ -5,3 +5,14 @@ var typed = new Typed(".about", {
 	loop: true,
 	showCursor: true,
 });
+
+// var handle = document.getElementsByClassName("handle");
+
+$(".lang-bar").on("click", ".handle", (event) => {
+	var switcher = document.getElementsByClassName("lang-bar")[0];
+	if (switcher.style.right !== "0px") {
+		switcher.style.right = "0px";
+	} else {
+		switcher.style.right = "-150px";
+	}
+});
