2a959db8364f0048cadb33c7839bc34793c20025
Réka Hajdú <hajdu.reka1109@gmail.com>
2024-04-21 10:54:24 +0200

    switch fix

--- main.js
+++ main.js
@@ -6,13 +6,25 @@ var typed = new Typed(".about", {
 	showCursor: true,
 });
 
-// var handle = document.getElementsByClassName("handle");
-
 $(".lang-bar").on("click", ".handle", (event) => {
 	var switcher = document.getElementsByClassName("lang-bar")[0];
 	if (switcher.style.right !== "0px") {
 		switcher.style.right = "0px";
 	} else {
 		switcher.style.right = "-150px";
+	}
+});
+$(".lang-body").on("click", "#languageSwitcher", (event) => {
+	var name = document.getElementById("name");
+	var description = document.getElementById("description");
+	if ($("#languageSwitcher").is(":checked")) {
+		//ANGOL VERZIÓ
+		console.log("Checkbox is checked");
+		name.textContent = "Réka Hajdú";
+		description.textContent = "I graduated in February of 2024 from... ";
+	} else {
+		//MAGYAR VERZÓ
+		console.log("Checkbox is not checked");
+		name.textContent = "Hajdú Réka";
 	}
 });
